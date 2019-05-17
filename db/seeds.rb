6bb8d704e83b49e83e97a9ad684413ff6b0a7c9a
Alexandre Chelednik <cheleds15@gmail.com>
2019-05-17 11:36:22 +0200

    not working yet

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,19 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+# frozen_string_literal: true
+
+require 'open-uri'
+require 'json'
+
+url = 'https://www.thecocktaildb.com/api/json/v1/1/list.php?i=list'
+img_url = 'https://www.thecocktaildb.com/images/ingredients/'
+
+html_content = open(url).read
+doc = JSON.parse(html_content)
+ingredients = doc['drinks']
+Ingredient.destroy_all
+
+ingredients.each do |i|
+  Ingredient.create!(
+    name: i['strIngredient1'],
+    image_url: "#{img_url}#{i['strIngredient1']}.png"
+  )
+end
